51099503afab632c4f386fa2e907da62ea66c588
namke-dev <namke.dev@gmail.com>
2024-01-18 04:02:10 +0700

    implement GetPlatformById and CreatePlatform endpoint

--- Controllers/PlatformController.cs
+++ Controllers/PlatformController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Dtos;
+using PlatformService.Models;
 using PlatformService.Repositories;
 
 namespace PlatformService.Controllers
@@ -23,6 +24,30 @@ namespace PlatformService.Controllers
         {
             var platformItem = _repository.GetAllPlatforms();
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem is not null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CeatePlatform(platformModel);
+            _repository.SaveChange();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { id = platformModel.Id }, platformReadDto);
         }
     }
 }
